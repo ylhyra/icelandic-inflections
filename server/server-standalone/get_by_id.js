618ba934aa3e7f782866a10ada6ce6d73fccf1a7
Egill <egill@egill.xyz>
2020-08-15 22:29:42 +0000

    no message

--- server/server-standalone/get_by_id.js
+++ server/server-standalone/get_by_id.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 export default (id, callback) => {
   axios.get(`https://ylhyra.is/api/inflection?id=${id}&type=flat`)
     .then(function({ data }) {
-      callback(data)
+      console.log(data.results)
+      callback(data.results)
     })
     .catch(function(error) {
       callback(null)
