9f0df28675a6151f590b6dd0081c40c74d0f7afd
Egill <egill@egill.xyz>
2020-09-03 14:34:40 +0000

    no message

---
+++ tables/tables_single.js
@@ -0,0 +1,29 @@
+import link, { ucfirst } from './link'
+import { RenderTable } from './render_table'
+import { without } from 'lodash'
+import { tags } from './classify'
+
+/**
+ * Finds a single relevant table
+ *
+ * @memberof Word
+ * @return {string} HTML as string
+ */
+export default function getSingleTable() {
+  const word = this
+  let description = ''
+  let table = ''
+  /* Nouns */
+  if (word.is('noun')) {
+    const sibling_classification = without(word.getFirstClassification(), ...tags['cases'])
+    const siblings = word.getOriginal().get(...sibling_classification)
+    table = RenderTable(siblings, null, {
+      column_names: [word.getType('article')],
+      row_names: tags['cases'],
+    }, word.getFirstClassification())
+    description = without(sibling_classification, ...tags['articles']).join(', ')
+  }
+
+
+  return description + table
+}
