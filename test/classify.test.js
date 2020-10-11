374b735baf16f1a2dde96eb26a75ec6a15f47a9c
Egill <egill@egill.xyz>
2020-10-11 17:49:42 +0000

    no message

---
+++ test/classify.test.js
@@ -0,0 +1,8 @@
+import assert from 'assert'
+import classify from './classification/BIN_classification'
+
+// it('splitOnVowels', () => {
+//   assert.deepEqual(splitOnVowels('Kjartan'), ['K', 'ja', 'rt', 'a', 'n'])
+//   assert.deepEqual(splitOnVowels('andrés'), ['', 'a', 'ndr', 'é', 's'])
+//   return
+// })
